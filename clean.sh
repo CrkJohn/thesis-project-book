ae522d78219767cda61361458f9462607d23356b
Alejandro Anzola Avila <anzola95avila@gmail.com>
2019-05-17 16:24:26 -0500

    no hacer commit

---
+++ clean.sh
@@ -0,0 +1,5 @@
+# Clear out junk
+find -type f -iname '*aux' -delete
+find -type f -iname '*.ind' -delete
+find -type f -iname '*.toc' -delete
+find -type f -iname '*.bbl' -delete
--- make.sh
+++ make.sh
@@ -1,11 +1,5 @@
 set -e
 
-# Clear out junk
-find -type f -iname '*aux' -delete
-find -type f -iname '*.ind' -delete
-find -type f -iname '*.toc' -delete
-find -type f -iname '*.bbl' -delete
-
 pdflatex main
 bibtex main
 # makeindex main

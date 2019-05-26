7f7c56793cb4eb357fdfd1bd75799d6491992772
Alejandro Anzola Avila <anzola95avila@gmail.com>
2019-05-25 20:54:02 -0500

    arregla script de limpieza para no daniar repositorio de git

--- clean.sh
+++ clean.sh
@@ -1,20 +1,20 @@
 # Clear out junk
 # find -type f -iname '*.bib' -delete
-find -type f -iname '*.acn' -delete
-find -type f -iname '*.acr' -delete
-find -type f -iname '*.alg' -delete
-find -type f -iname '*.aux' -delete
-find -type f -iname '*.bbl' -delete
-find -type f -iname '*.blg' -delete
-find -type f -iname '*.glg' -delete
-find -type f -iname '*.glo' -delete
-find -type f -iname '*.gls' -delete
-find -type f -iname '*.idx' -delete
-find -type f -iname '*.ilg' -delete
-find -type f -iname '*.ist' -delete
-find -type f -iname '*.lof' -delete
-find -type f -iname '*.log' -delete
-find -type f -iname '*.lot' -delete
-find -type f -iname '*.out' -delete
-find -type f -iname '*.xml' -delete
-find -type f -iname '*.toc' -delete
+find -type f -iname '*.acn' -not -path '*/\.*' -delete
+find -type f -iname '*.acr' -not -path '*/\.*' -delete
+find -type f -iname '*.alg' -not -path '*/\.*' -delete
+find -type f -iname '*.aux' -not -path '*/\.*' -delete
+find -type f -iname '*.bbl' -not -path '*/\.*' -delete
+find -type f -iname '*.blg' -not -path '*/\.*' -delete
+find -type f -iname '*.glg' -not -path '*/\.*' -delete
+find -type f -iname '*.glo' -not -path '*/\.*' -delete
+find -type f -iname '*.gls' -not -path '*/\.*' -delete
+find -type f -iname '*.idx' -not -path '*/\.*' -delete
+find -type f -iname '*.ilg' -not -path '*/\.*' -delete
+find -type f -iname '*.ist' -not -path '*/\.*' -delete
+find -type f -iname '*.lof' -not -path '*/\.*' -delete
+find -type f -iname '*.log' -not -path '*/\.*' -delete
+find -type f -iname '*.lot' -not -path '*/\.*' -delete
+find -type f -iname '*.out' -not -path '*/\.*' -delete
+find -type f -iname '*.xml' -not -path '*/\.*' -delete
+find -type f -iname '*.toc' -not -path '*/\.*' -delete
